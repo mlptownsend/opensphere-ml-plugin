82cbd781e0d860115d798cbae16e4df62393fdcd
Peter Townsend <peter.townsend@maplarge.com>
2018-04-19 19:17:00 -0500

    Attempt

---
+++ externs/ml.js
@@ -0,0 +1,23 @@
+var ml = {
+ "$": function(x) {},
+ onload: function(x) {},
+ ui: {
+  map: {
+   Map: function() {},
+   editor: {
+    MapEditor: function() {}
+   }
+  }
+ }
+};
+ml.ui.map.Map.prototype = {
+ "constructor": function(a,b,c) {},
+ "onInternalMapAvailable": function(x) {},
+ "zzMap": {
+  get: function() {}
+ }
+};
+ml.ui.map.editor.MapEditor.prototype = {
+ "constructor": function(x) {},
+ "show": function() {}
+};
